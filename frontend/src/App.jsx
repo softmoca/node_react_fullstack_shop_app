64ea748f6306227090cc6307d5e0708c49913d04
softmoca <acyc0909@gmail.com>
2023-08-08 21:26:26 +0900

    feat : router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./layout/NavBar";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Footer from "./layout/Footer";
+
+function Layout() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">hello moca</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/*로그인과 상관없이 갈 수 있는 경로 */}
+        <Route index element={<LandingPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 }
 
--- frontend/src/pages/LandingPage/index.jsx
+++ frontend/src/pages/LandingPage/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function LandingPage() {
-  return <div></div>;
+  return <div>LandingPage</div>;
 }
