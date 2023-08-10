cdcdaac02359f3d89423b126dd82d1d5d3b1934f
softmoca <acyc0909@gmail.com>
2023-08-10 19:46:43 +0900

    feat : connect mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
 
 const app = express();
 const port = 4000;
 
-
-
-app.get(
-  "/",
-  (req,
-  res) => {
-    res.send("gd");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("연결 완료");
   })
-;
+  .catch((err) => {
+    console.error(err);
+  });
 
-app.listen(port,()=>{console.log(`${port}번에서 실행이 되었습니다!.`)});
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("gd");
+});
+
+app.listen(port, () => {
+  console.log(`${port}번에서 실행이 되었습니다!.`);
+});
